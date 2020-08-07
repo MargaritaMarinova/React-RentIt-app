61449be51eb40e46c94a88e0ef2651319eb579fb
MargaritaMarinova <margarita.tomova@gmail.com>
2020-08-07 15:02:19 +0300

    feat: create register page

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,12 +1,67 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Title from '../../components/title'
+import SubmitButton from '../../components/button/submitButton'
+import styles from './index.module.css'
 import PageLayout from '../../components/page-layout'
+import Input from '../../components/input'
 
-const RegisterPage = () => {
-    return(
+
+class RegisterPage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state={
+            username: "",
+            password: "",
+            rePassword: ""
+        }
+    }
+
+    onChange = (event, type) => {
+        const newState = {}
+        newState[type] = event.target.value
+        this.setState(newState)
+    }
+
+    render () {
+        const {
+            username,
+            password,
+            rePassword
+        } = this.state
+    return (
         <PageLayout>
-        <p> Register Page</p>
+        <div className = {styles.container}>
+            <Title title = "Register" />
+            <Input
+            value={username}
+            onChange = {(e) => this.onChange(e, 'username')}
+            label="Username"
+            id="username"
+            />
+            <Input
+            value={password}
+            onChange = {(e) => this.onChange(e, 'password')}
+            label="Password"
+            id="password"
+            />
+            <Input
+            value={rePassword}
+            onChange = {(e) => this.onChange(e, 'rePassword')}
+            label="Re-password"
+            id="rePassword"
+            />
+
+
+            
+        </div>
+        <div className = {styles.button}>
+        <SubmitButton title = "Register" />
+        </div>
         </PageLayout>
     )
+
+}
 }
 
 export default RegisterPage
