e12bc9d6639996ecde2609fa8b9c0aa74561423d
MargaritaMarinova <margarita.tomova@gmail.com>
2020-08-08 10:26:12 +0300

    fix: update database and access

--- restapi/config/config.js
+++ restapi/config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 9999,
-        dbURL: 'mongodb+srv://rentitadmin:rentitpass@rentit.jcupy.mongodb.net/items?retryWrites=true&w=majority',
+        dbURL: 'mongodb+srv://rentitadmin:rentitpass@rentit.jcupy.mongodb.net/rentit?retryWrites=true&w=majority',
         authCookieName: 'x-auth-token'
     },
     production: {}
